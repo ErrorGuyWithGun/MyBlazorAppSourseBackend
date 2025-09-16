c7a45eda25355ebc3539340d5e6d1bb4a862d134
ErrorGuyWithGun <thevlad3323@gmail.com>
2025-09-16 10:55:03 +0300

    Deleted an unnecessary model

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c645bdb7213277d80c815a9d08ff45606c6d9ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6df07de0f2f4cc7d059ff758161ca1b8c6d1c8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
